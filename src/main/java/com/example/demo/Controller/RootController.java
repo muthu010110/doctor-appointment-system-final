a2e1eefa2bfab60cb1a47859e4f15201b9ffb73d
muthu010110 <muthukarunanithi2003@gmail.com>
2025-11-07 22:45:32 +0530

    done

--- src/main/java/com/example/demo/Controller/RootController.java
+++ src/main/java/com/example/demo/Controller/RootController.java
@@ -1,9 +1,13 @@
 package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class RootController {
-	 @GetMapping("/")
-	  public String ok(){ return "Doctor Appointment API is running ✅"; }
 
+    @GetMapping("/")
+    public String ok() {
+        return "Doctor Appointment API is running ✅";
+    }
 }
